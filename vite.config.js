766d351d0d15e80bef10ebd216a01c57750edddd
LIFI <123@outlook.com>
2022-03-24 15:58:45 +0800

    code review

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import Vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-const path = require('path')
+import path from 'path'
 
 // https://cn.vitejs.dev/
 export default defineConfig({
