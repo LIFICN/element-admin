eb1461894470f44f9530539442676a82b6fca3a6
LIFI <123@outlook.com>
2022-03-25 17:49:35 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//element plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+//nprogress
+import 'nprogress/nprogress.css'
+
+const app = createApp(App)
+app.use(ElementPlus, { locale: zhCn })
+app.mount('#app')
