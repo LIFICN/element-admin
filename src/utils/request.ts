07a3d989615d6f4f388744d7b26773d2f4dc2a7f
LIFI <123@outlook.com>
2022-03-30 14:35:44 +0800

    add request

---
+++ src/utils/request.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { getToken } from '@/utils/auth'
+
+// http://www.axios-js.com/zh-cn/docs/
+const instance = axios.create({
+    baseURL: '',
+    timeout: 3000,
+})
+
+instance.interceptors.request.use(
+    (config) => {
+        const token = getToken()
+        if (token) config!.headers!['X-Token'] = token
+        return config
+    },
+    (error) => Promise.reject<any>(error)
+)
+
+instance.interceptors.response.use(
+    (response) => response.data,
+    (error) => Promise.reject<any>(error)
+)
+
+export default instance
