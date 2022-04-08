f4213169a676b69c0578ec425e7a103a7907f559
LIFI <123@outlook.com>
2022-04-08 09:45:21 +0800

    remove packages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
-  <el-config-provider :locale="locale">
-    <router-view />
-  </el-config-provider>
+  <router-view />
 </template>
-
-<script setup>
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import { ref } from 'vue'
-
-const locale = ref(zhCn)
-</script>
 
 <style>
 @import url('@/styles/transition.scss');
