e88ca0ade5bd7eb06392eb0e6ffde6d2ef360581
LIFI <123@outlook.com>
2022-05-25 14:36:03 +0800

    alias fix

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,17 @@ import path from 'path'
 
 // https://cn.vitejs.dev/
 export default defineConfig({
-  plugins: [
-    Vue(),
-  ],
+  plugins: [Vue()],
   server: {
-    open: true,  //是否打开浏览器
-    port: 3000,  //端口号
-    cors: true,   //跨域
-    host: '0.0.0.0' //ip地址
+    open: true, //是否打开浏览器
+    port: 3000, //端口号
+    cors: true, //跨域
+    host: '0.0.0.0', //ip地址
   },
-  base: '/',  //根路径
+  base: '/', //根路径
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, "./src") // 转换 '@' to './src' 
-    }
-  }
+      '@': path.resolve(__dirname, 'src'), // 转换 '@' to 'src'
+    },
+  },
 })
